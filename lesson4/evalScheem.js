ec5a3258cb227c7574c5c8127c2f378a2b5d7d9a
Robert Seeger <RHSeeger@gmail.com>
2012-05-04 22:17:57 -0400

    step 3

--- lesson4/evalScheem.js
+++ lesson4/evalScheem.js
@@ -25,29 +25,47 @@ var evalScheem = function (expr, env) {
     }
     // Look at head of list for operation
     switch (expr[0]) {
-        case '+':
-            return evalScheem(expr[1], env) + evalScheem(expr[2], env);
-        case '-':
-            return evalScheem(expr[1], env) - evalScheem(expr[2], env);
-        case '*':
-            return evalScheem(expr[1], env) * evalScheem(expr[2], env);
-        case '/':
-            return evalScheem(expr[1], env) / evalScheem(expr[2], env);
+    case '+':
+        return evalScheem(expr[1], env) + evalScheem(expr[2], env);
+    case '-':
+        return evalScheem(expr[1], env) - evalScheem(expr[2], env);
+    case '*':
+        return evalScheem(expr[1], env) * evalScheem(expr[2], env);
+    case '/':
+        return evalScheem(expr[1], env) / evalScheem(expr[2], env);
+    case 'set!':
+        env[expr[1]] = evalScheem(expr[2], env);
+        return 0;
+    case 'define':
+        env[expr[1]] = evalScheem(expr[2], env);
+        return 0;
     }
 };
 
 
-// Stage 1
+/* Stage 1 */
 assert_eq(evalScheem(5), 5,    '5 test');
 assert_eq(evalScheem(['+', 2, 3]), 5,    '(+ 2 3) test');
 assert_eq(evalScheem(['*', 2, 3]), 6,    '(* 2 3) test');
 assert_eq(evalScheem(['/', 1, 2]), 0.5,    '(/ 1 2) test');
 assert_eq(evalScheem(['*', ['/', 8, 4], ['+', 1, 1]]), 4,    '(* (/ 8 4) (+ 1 1)) test');
 
-// Stage 2
+/* Stage 2 */
 var env = {x:2, y:3, z:10};
 assert_eq(evalScheem(5, env), 5,    '5 test');
 assert_eq(evalScheem('x', env), 2,    'x test');
 assert_eq(evalScheem(['+', 2, 3], env), 5,    '(+ 2 3) test');
 assert_eq(evalScheem(['*', 'y', 3], env), 9,    '(* y 3) test');
 assert_eq(evalScheem(['/', 'z', ['+', 'x', 'y']], env), 2,    '(/ z (+ x y)) test');
+
+/* Stage 3 */
+assert_eq(evalScheem('x', env), 2,    'x test');
+var tmp = evalScheem(['define', 'a', 5], env);
+assert_eq(tmp, 0,    'evaluation of define test');
+assert_eq(env, {x:2, y:3, z:10, a:5},    '(define a 5) test');
+var tmp = evalScheem(['set!', 'a', 1], env);
+assert_eq(env, {x:2, y:3, z:10, a:1},    '(set! a 1) test');
+var tmp = evalScheem(['set!', 'x', 7], env);
+assert_eq(env, {x:7, y:3, z:10, a:1},    '(set! x 7) test');
+var tmp = evalScheem(['set!', 'y', ['+', 'x', 1]], env);
+assert_eq(env, {x:7, y:8, z:10, a:1},    '(set! y (+ x 1)) test');
