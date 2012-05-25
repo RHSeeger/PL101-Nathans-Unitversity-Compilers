99c9919909a082bcf70bc266a12b73ea442bb5b8
Robert Seeger <RHSeeger@gmail.com>
2012-05-24 23:41:09 -0400

    added ljoin

--- lesson7/test/functionsTest.js
+++ lesson7/test/functionsTest.js
@@ -13,8 +13,8 @@ if (typeof module !== 'undefined') {
  * TESTS
  */
 
-suite("SUM", function() {
-    suite("normal recursive", function() {
+suite("SUM -", function() {
+    suite("normal recursive -", function() {
         test('base', function() {
             assert.deepEqual(funcs.sum.normal(1), 1);
         });
@@ -22,7 +22,7 @@ suite("SUM", function() {
             assert.deepEqual(funcs.sum.normal(5), 15);
         });
     });
-    suite("cps", function() {
+    suite("cps -", function() {
         test('base', function() {
             assert.deepEqual(funcs.sum.cps(1), 1);
         });
@@ -30,12 +30,39 @@ suite("SUM", function() {
             assert.deepEqual(funcs.sum.cps(5), 15);
         });
     });
-    suite("normal recursive", function() {
+    suite("thunked -", function() {
         test('base', function() {
             assert.deepEqual(funcs.sum.thunked(1), 1);
         });
         test('normal', function() {
             assert.deepEqual(funcs.sum.thunked(5), 15);
+        });
+    });
+});
+
+suite("LJOIN -", function() {
+    suite("normal recursive -", function() {
+        test('base', function() {
+            assert.deepEqual(funcs.ljoin.normal([], "-"), "");
+        });
+        test('normal', function() {
+            assert.deepEqual(funcs.ljoin.normal([1,2,3,4,5], "-"), "1-2-3-4-5");
+        });
+    });
+    suite("cps -", function() {
+        test('base', function() {
+            assert.deepEqual(funcs.ljoin.cps([], "-"), "");
+        });
+        test('normal', function() {
+            assert.deepEqual(funcs.ljoin.cps([1,2,3,4,5], "-"), "1-2-3-4-5");
+        });
+    });
+    suite("thunked -", function() {
+        test('base', function() {
+            assert.deepEqual(funcs.ljoin.thunked([], "-"), "");
+        });
+        test('normal', function() {
+            assert.deepEqual(funcs.ljoin.thunked([1,2,3,4,5], "-"), "1-2-3-4-5");
         });
     });
 });
